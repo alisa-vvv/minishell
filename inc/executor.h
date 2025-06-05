f03d98a28b8bc639591d87d8978de8890a6b5b45
Alisa Valiullina <avaliull@f1r6s5.codam.nl>
2025-06-05 17:54:48 +0200

    trying to figure out proper chaining. something seems to break when the alst command needs input from a write pipe end

--- inc/executor.h
+++ inc/executor.h
@@ -19,10 +19,16 @@ typedef struct	s_command_io
 {
 	int	in_fd;
 	int	out_fd;
+	int	in_pipe[2];
+	int	out_pipe[2];
 }	t_command_io;
 
 /*		Executor - temporary functions!		*/
 t_exec_data	*test_get_dummy_exec_data();
 int	executor(t_exec_data *exec_data, int dummy_minishell_struct);
+
+/*		Try_execve		*/
+char	**find_env_path(void);
+int		try_execve(const char **path, char *const argv[]);
 
 #endif
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -17,29 +17,49 @@
 #include <errno.h>
 #include <fcntl.h>
 
+// TEST
 t_exec_data	*test_get_dummy_exec_data(void)
 {
 	t_exec_data	*exec_data;
 
+	int i = 0;
+	int len = 3;
 	exec_data = ft_calloc(3, sizeof(t_exec_data));
-	exec_data[0].len = 2;
-	exec_data[0].argv = ft_calloc(3, sizeof(char *));
-	exec_data[0].argv[0] = ft_strdup("cat");
-	exec_data[0].argv[1] = ft_strdup("-b");
-	exec_data[0].input_type = custom_fd;
-	exec_data[0].output_type = pipe_write;
-	exec_data[0].in_filename = ft_strdup("infile");
-	exec_data[1].len = 2;
-	exec_data[1].argv = ft_calloc(3, sizeof(char *));
-	exec_data[1].argv[0] = ft_strdup("cat");
-	exec_data[1].argv[1] = ft_strdup("-e");
-	exec_data[1].input_type = pipe_read;
-	exec_data[1].output_type = custom_fd;
-	exec_data[1].redirect_type = append;
-	exec_data[1].out_filename = ft_strdup("outfile");
+	exec_data[i].len = len;
+	exec_data[i].argv = ft_calloc(exec_data[i].len + 1, sizeof(char *));
+	exec_data[i].argv[0] = ft_strdup("cat");
+	exec_data[i].argv[1] = NULL;
+	exec_data[i].argv[2] = NULL;
+	exec_data[i].input_type = custom_fd;
+	exec_data[i].output_type = pipe_write;
+	exec_data[i].in_filename = ft_strdup("infile");
+	i++;
+
+	exec_data[i].len = len;
+	exec_data[i].argv = ft_calloc(exec_data[i].len + 1, sizeof(char *));
+	exec_data[i].argv[0] = ft_strdup("cat");
+	exec_data[i].argv[1] = ft_strdup("-e");
+	exec_data[i].argv[2] = NULL;
+	exec_data[i].input_type = pipe_read;
+	exec_data[i].output_type = pipe_write;
+	exec_data[i].redirect_type = append;
+	exec_data[i].out_filename = ft_strdup("midfile");
+	i++;
+
+	exec_data[i].len = len;
+	exec_data[i].argv = ft_calloc(exec_data[i].len + 1, sizeof(char *));
+	exec_data[i].argv[0] = ft_strdup("cat");
+	exec_data[i].argv[1] = ft_strdup("-b");
+	exec_data[i].argv[2] = NULL;
+	exec_data[i].input_type = pipe_read;
+	exec_data[i].output_type = custom_fd;
+	exec_data[i].redirect_type = append;
+	exec_data[i].out_filename = ft_strdup("outfile");
+	i++;
 	return (exec_data);
 }
 
+// IO CHAIN
 void	set_in_fd(t_exec_data *command, int *in_fd)
 {
 	if (command->input_type == custom_fd)
@@ -52,25 +72,14 @@ void	set_out_fd(t_exec_data *command, int *out_fd)
 {
 	if (command->output_type == custom_fd
 		&& command->redirect_type == trunc)
-		*out_fd = open(command->out_filename, O_WRONLY | O_CREAT | O_TRUNC);
+		*out_fd = open(command->out_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	else if (command->output_type == custom_fd
 		&& command->redirect_type == append)
-		*out_fd = open(command->out_filename, O_WRONLY | O_CREAT | O_APPEND);
+		*out_fd = open(command->out_filename, O_WRONLY | O_CREAT | O_APPEND, 0664);
 	else if (command->output_type == std_out)
 		*out_fd = STDOUT_FILENO;
 	else if (command->output_type == std_err)
 		*out_fd = STDERR_FILENO;
-}
-
-int	spawn_children(t_exec_data *command, t_command_io *command_io, int i)
-{
-	printf("command: %s\n", command->argv[0]);
-	printf("input type: %d\n", command->input_type);
-	printf("command in_fd: %d\n", command_io[i].in_fd);
-	printf("output type: %d\n", command->output_type);
-	printf("command out_fd: %d\n", command_io[i].out_fd);
-	printf("placeholder - child process created\n");
-	return (0);
 }
 
 int	prepare_io_chain(t_exec_data *command, t_command_io *command_io, int i)
@@ -88,16 +97,16 @@ int	prepare_io_chain(t_exec_data *command, t_command_io *command_io, int i)
 
 	if (command->output_type == pipe_write)
 	{
-		err_check = pipe(
-				(int [2]){
-				command_io[i + 1].in_fd,
-				command_io[i].out_fd
-			}
-		);
+		err_check = pipe(command_io[i].out_pipe);
 		if (err_check < 0)
-			perror("pipe errored");
-		printf("pipe read: %d\n", command_io[i + 1].in_fd);
-		printf("pipe write: %d\n", command_io[i].out_fd);
+		{
+			// ADD ERROR MANAGEMENT!!
+			printf("PLACEHOLDER, THIS SHOULD ERROR\n");
+		}
+		command_io[i].out_fd = command_io[i].out_pipe[1];
+		command_io[i + 1].in_fd = command_io[i].out_pipe[0];
+		command_io[i + 1].in_pipe[0] = command_io[i].out_pipe[0];
+		command_io[i + 1].in_pipe[1] = command_io[i].out_pipe[1];
 	}
 	else
 		set_out_fd(command, &command_io[i].out_fd);
@@ -109,16 +118,79 @@ int	prepare_io_chain(t_exec_data *command, t_command_io *command_io, int i)
 	return (err_check);
 }
 
-// I think I need to just iteratively setup pipes in the parent process
-// then the next iteration would actually split the process into children since now we have pipe pairs
-// and execution can start from 0 to N
-// and if there's no pipes then it jsut uses whatever the in/out fd value is
-// doest that regardless I think?
-// probably special casse for heredoc but would need to research
-//
+// CHILDREN
+int	spawn_children(t_exec_data *command, t_command_io *command_io, int i)
+{
+	char	**path_var;
+	pid_t	process_id;
+//	printf("command: %s\n", command->argv[0]);
+//	printf("input type: %d\n", command->input_type);
+//	printf("command in_fd: %d\n", command_io[i].in_fd);
+//	printf("output type: %d\n", command->output_type);
+//	printf("command out_fd: %d\n", command_io[i].out_fd);
+	path_var = find_env_path();
+	process_id = fork();
+	if (process_id == 0)
+	{
+		printf("i: %d\n", i);
+		if (command->input_type == pipe_read)
+		{
+			printf("inpipe 0: %d\n", command_io[i].in_pipe[0]);
+			printf("inpipe 1: %d\n", command_io[i].in_pipe[1]);
+			dup2(command_io[i].in_pipe[0], STDIN_FILENO);
+			close(command_io[i].in_pipe[1]);
+		}
+		else if (command->input_type != std_in)
+			dup2(command_io[i].in_fd, STDIN_FILENO);
+
+		if (command->output_type == pipe_write)
+		{
+			printf("outpipe 1: %d\n", command_io[i].out_pipe[1]);
+			printf("outpipe 0: %d\n", command_io[i].out_pipe[0]);
+			printf("next inpipe 1: %d\n", command_io[i + 1].in_pipe[1]);
+			printf("next inpipe 0: %d\n", command_io[i + 1].in_pipe[0]);
+			dup2(command_io[i].out_pipe[1], STDOUT_FILENO);
+			close(command_io[i].out_pipe[0]);
+		}
+		else if (command->output_type != std_out)
+		{
+			printf("what?\n");
+			dup2(command_io[i].out_fd, STDOUT_FILENO);
+		}
+		try_execve((const char **) path_var, command->argv);
+		exit(9999);
+	}
+	else if (process_id > 0)
+	{
+		if (command->input_type == pipe_read)
+		{
+			close(command_io[i].in_pipe[1]);
+			close(command_io[i].in_pipe[0]);
+		}
+		if (command->output_type == pipe_write)
+		{
+			close(command_io[i].out_pipe[1]);
+			close(command_io[i].out_pipe[0]);
+		}
+//		if ((command)->output_type == pipe_write
+//			&& (command + 1)->input_type == pipe_read)
+//			waitpid(process_id, NULL, 0);
+//		printf("this wait?\n");
+		return (0);
+	}
+	else
+	{
+		// ADD ERROR MANAGEMENT!!
+		printf("PLACEHOLDER, THIS SHOULD ERROR\n");
+		return (1);
+	}
+	return (0);
+}
+
 int	executor(t_exec_data *exec_data, int dummy_minishell_struct)
 {
 	int				i;
+	int				process_status;
 	t_command_io	*io_chain;
 
 	io_chain = ft_calloc(exec_data->len, sizeof(t_command_io));
@@ -128,5 +200,7 @@ int	executor(t_exec_data *exec_data, int dummy_minishell_struct)
 	i = -1;
 	while (++i < exec_data->len)
 		spawn_children(&exec_data[i], io_chain, i);
+	wait(&process_status);
+	printf("we have waited\n");
 	return (1);
 }
