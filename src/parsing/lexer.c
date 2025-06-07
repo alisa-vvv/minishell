f98b7ef67fedac7896736467487e320d8a9229bb
Anna Dechering <adecheri@f1r1s13.codam.nl>
2025-06-07 13:34:14 +0200

    start lexer with ll later vect

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -20,6 +20,29 @@ int is_EOS(char *str, int i)
     return (0);
 }
 
+void	ft_safefree(void *ptr)
+{
+	if (ptr)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+	return ;
+}
+
+void	ft_freearr(void **array)
+{
+	int	i;
+
+	if (array)
+	{
+		i = -1;
+		while (array[++i])
+			ft_safefree(array[i]);
+		ft_safefree(array);
+	}
+}
+
 void push_token(t_token *token, t_token **tlist)
 {
     t_token *last;
@@ -33,39 +56,90 @@ void push_token(t_token *token, t_token **tlist)
     last = token; 
 }
 
-t_token *new_token(char *str)
+t_token *new_token(char *str, int pos, int len)
 {
     t_token *token;
     token = ft_calloc(1, sizeof(t_token));
     if (!token)
         return (MALLOC_ERR);
-    token->value = ft_strdup(str);
-    token->type = match(str);
+    token->value = ft_strlcpy(token->value, str[pos], len);
+    if (!token->value)
+        return (MALLOC_ERR);
+    token->type = match(token->value);
     return (token);
 }
 
-int default_lexer(char *str)
+int token_count(char *str)
 {
-    int i;
-    int j;
-    i = 0;
-    j = 0;
-    //intoken = 0; 
-    char *input_line; 
-    input_line = readline("minishell");
+    int tokencount;
+    tokencount = 0;
+
+    while (*str)
+    {
+        if (ft_isspace(*str))
+            str++;
+        else
+        {
+            tokencount++;
+            while (!ft_isspace(*str) && *str)
+            {
+                str++;
+            }
+        }
+    }
+    return (tokencount);
+}
+
+void ft_clear_list(t_token **list)
+{
+    while ((*list)->next)
+        ft_safefree()
+
+}
+
+int fill_tokenlist(t_token **tokenlist, char *str)
+{
+    size_t len;
+    t_token *token;
+
+    while (*str)
+    {
+        len = 0;
+        while (*str && ft_isspace(*str))
+            str++;
+        while (*str && !ft_isspace(*str))
+        {
+            len++;
+            str++;
+        }
+        if (len > 0)
+        {
+            token = new_token(str, str - len, len + 1);
+            if (!token)
+            {
+                ft_clear_list(tokenlist);
+                return (MALLOC_ERR);
+            }
+            push_token(token, tokenlist);
+        }
+    }
+    return (0);
+}
+
+
+int default_lexer(char *input_line)
+{
+    int token_c;
+    t_token *token_list;
+    //input_line = readline("minishell");
     if (!input_line)
         return (1);
-    while (!is_EOS(input_line, i))
-    {
-        while (ft_isspace(input_line[i]))
-            i++;
-        while (ft_isprint(input_line[i]) && !ft_isspace(input_line))
-        {
-            j++;
-        }
-        
-        
-    }
+    token_c = token_count(input_line);
+    token_list = ft_malloc();
+    if (!token_list)
+        return (NULL);
+    
+
     return (0);
 }
 
@@ -78,7 +152,6 @@ int match_token(char *str_read)
     else if (str_read[i] == '"')
 
 }
-
 
 // t_token *regular_expression(char *str)
 // {
