caa18d8f0163c120c1bfd4d9b64264a5ade51f0d
adecheri <adecheri@student.codam.nl>
2025-11-02 17:04:26 +0100

    expansions need a little love for 4 errors, but should be leak free at least

--- inc/vec_lib.h
+++ inc/vec_lib.h
@@ -16,42 +16,42 @@
 # include <stddef.h>
 
 
-typedef struct s_elementlist
-{
-	void					**tokens;
-	size_t size; // in mem
-	size_t					total;
-}							t_elementlist;
+	typedef struct s_elementlist
+	{
+		void					**tokens;
+		size_t size; // in mem
+		size_t					total;
+	}							t_elementlist;
 
-typedef struct s_element	element;
+	typedef struct s_element	element;
 
-struct						s_element
-{
-	t_elementlist			element_list;
-	int						(*pf_element_total)(element *);
-	int						(*pf_element_resize)(element *, int, int);
-	int						(*pf_element_add)(element *, void *);
+	struct						s_element
+	{
+		t_elementlist			element_list;
+		int						(*pf_element_total)(element *);
+		int						(*pf_element_resize)(element *, int, int);
+		int						(*pf_element_add)(element *, void *);
 
-	int						(*pf_element_set)(element *, size_t, void *);
-	void					*(*pf_element_get)(element *, size_t);
-	int						(*pf_element_delete)(element *, size_t);
-	int						(*pf_element_free)(element *);
-	// int						(*pf_element_swap)(element *, void *, size_t pos1,
-	// 							size_t pos2);
-	// int						(*pf_element_insert)(element *, void *, int pos);
-};
+		int						(*pf_element_set)(element *, size_t, void *);
+		void					*(*pf_element_get)(element *, size_t);
+		int						(*pf_element_delete)(element *, size_t);
+		int						(*pf_element_free)(element *);
+		// int						(*pf_element_swap)(element *, void *, size_t pos1,
+		// 							size_t pos2);
+		// int						(*pf_element_insert)(element *, void *, int pos);
+	};
 
-int							element_total(element *e);
-int							element_resize(element *e, int oldsize,
-								int newsize);
-int							element_push_back(element *e, void *token);
-int							element_set(element *e, size_t index, void *token);
-void						*element_get(element *e, size_t index);
-int							element_delete(element *e, size_t index);
-int							element_free(element *e);
-void						element_init(element *e, int size);
-// int							element_swap(element *e, void *token, size_t pos1,
-// 								size_t pos2);
-// int							element_insert(element *e, void *token, int pos);
+	int							element_total(element *e);
+	int							element_resize(element *e, int oldsize,
+									int newsize);
+	int							element_push_back(element *e, void *token);
+	int							element_set(element *e, size_t index, void *token);
+	void						*element_get(element *e, size_t index);
+	int							element_delete(element *e, size_t index);
+	int							element_free(element *e);
+	void						element_init(element *e, int size);
+	// int							element_swap(element *e, void *token, size_t pos1,
+	// 								size_t pos2);
+	// int							element_insert(element *e, void *token, int pos);
 
 #endif
--- src/parsing/expansions.c
+++ src/parsing/expansions.c
@@ -28,21 +28,17 @@
 // look for return value in minishell struct when accessing $?
 
 //expand the var when token is in quotes 
-char *exp_str_token(char *str_token, char *name, char *value)
+char *exp_str_token(char *str_token, char *value, int offset)
 {
     char *new_str;
     char *start; 
     char *leftover;
     char *temp_left;
-    int offset; 
 
-    if (!value || !name)
-        value = "";
     // start = NULL;
     // leftover = NULL;
-    new_str = NULL;
+    // new_str = NULL;
     start = ft_strchr(str_token, '$');
-    offset = ft_strlen(name) + 1;
     temp_left = malloc(sizeof(char) * ft_strlen(start) - offset);
     if (!temp_left)
         return (NULL);
@@ -78,7 +74,8 @@ char *refine_name_var(char *token_name, char *result)
         i++;
     }
     result[i] = '\0';
-    ft_safe_free((unsigned char **)&start);
+    if (ft_strncmp(start, "\0", 1) != 0)
+        ft_safe_free((unsigned char **)&start);
     return (result);
 }
 
@@ -91,6 +88,15 @@ void expand_unquoted(element *tokenlist, t_token *check_token, int pos, char *en
     index_lexer(&tokenlist);
 }
 
+void expand_quoted(t_token *check_token, char *name, char *env_value)
+{
+    int offset; 
+
+    if (!env_value || !name)
+        env_value = "";
+    offset = ft_strlen(name) + 1;
+    check_token->value = exp_str_token(check_token->value, env_value, offset);
+}
 
 //expand known var and otherwise delete and re-position all tokens
 int expand_var(element **tokenlist, 
@@ -108,12 +114,8 @@ int expand_var(element **tokenlist,
         printf("%d\n", (*minishell_data)->last_pipeline_return);
     env_value = env_var_get_value((*minishell_data)->env, name);
     e_printf("\nNAME= %s \n", name);
-    if (quoted && env_value)
-    {
-        e_printf("VALUE= %s \n", env_value);
-        check_token->value = exp_str_token(check_token->value, name, env_value);
-        ft_safe_free((unsigned char **)&env_value);
-    }
+    if (quoted)
+        expand_quoted(check_token, name, env_value);
     else if (!quoted && env_value)
         expand_unquoted(*tokenlist, check_token, pos, env_value);
     else if (!quoted && !env_value)
